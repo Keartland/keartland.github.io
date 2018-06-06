6da97acb3cbffd76843e41f860708f813cbf89e9
Harry Keartland <harrykeartland@gmail.com>
2018-06-06 19:49:52 +0100

    Added boxes enlarging

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 var request = new Request('https://api.github.com/zen', {
 	headers: new Headers({})
 });
-links = []
+info = {}
 fetch("https://api.github.com/users/keartland/repos")
 .then(r => r.json())
   .then(function(data){
@@ -11,14 +11,22 @@ fetch("https://api.github.com/users/keartland/repos")
       lang = cur["language"]
       if(lang =="JavaScript" || lang =="CSS"|| lang =="HTML"){
 				if (cur["name"]!="keartland.github.io"){
-        	document.getElementById("repos").innerHTML += "<div onclick=\"run(event)\" class=\"row\"><h1>" + cur["name"].replace(/-/g," ") + "</h1></div>"
-					links.push("https://keartland.github.io/"+cur["name"])
+        	document.getElementById("repos").innerHTML += "<div onclick=\"run(this.id)\" id=\"" + cur["name"] +"\" class=\"row\"><h1>" + cur["name"].replace(/-/g," ") + "</h1></div>"
+					info[cur["name"]] = {"desc":cur["description"], "url":"https://keartland.github.io/"+cur["name"] }
       	}
     	}
 		}
 	})
-function run(event){
-	mouseY = event.clientY - 70;
-	window.location = links[Math.floor(mouseY/90)];
-	console.log(links[Math.floor(mouseY/90)])
+
+function run(id){
+	var ele = document.getElementById(id)
+	add = "<h2>" + info[id]["desc"] + "</h2><a href=\"" + info[id]["url"] +"\">GOTO</a>"
+	if(ele.className == "row"){
+		temp = ele.innerHTML
+		ele.setAttribute('class', 'active');
+		ele.innerHTML = temp + add
+	}else if(ele.className == "active"){
+		ele.setAttribute('class', 'row');
+		ele.innerHTML = ele.innerHTML.replace(add,"")
+	}
 }
