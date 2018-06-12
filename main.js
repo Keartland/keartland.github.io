931d32d552fa0e69a962ee27805858cff82569eb
Harry Keartland <harrykeartland@gmail.com>
2018-06-12 22:54:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,20 +2,20 @@ info = {}
 fetch("https://api.github.com/users/keartland/repos")
 .then(r => r.json())
   .then(function(data){
-		console.log("loaded")
-		console.log(data.length)
-    for (var i = 0; i < data.length; i++) {
-      cur = data[i]
-      lang = cur["language"]
-			console.log(cur["name"])
-      if(lang =="JavaScript" || lang =="CSS"|| lang =="HTML"){
-				if (cur["name"]!="keartland.github.io"){
-        	document.getElementById("repos").innerHTML += "<div onclick=\"run(this.id)\" id=\"" + cur["name"] +"\" class=\"row\"><h1 class=\"titles\">" + cur["name"].replace(/-/g," ") + "</h1></div>"
-					info[cur["name"]] = {"desc":cur["description"], "url":"https://keartland.github.io/"+cur["name"] }
-      	}
-    	}
+	console.log("loaded")
+	console.log(data.length)
+    	for (var i = 0; i < data.length; i++) {
+      		cur = data[i]
+      		lang = cur["language"]
+		console.log(cur["name"])
+      		if(lang =="JavaScript" || lang =="CSS"|| lang =="HTML"){
+			if (cur["name"]!="keartland.github.io"){
+        			document.getElementById("repos").innerHTML += "<div onclick=\"run(this.id)\" id=\"" + cur["name"] +"\" class=\"row\"><h1 class=\"titles\">" + cur["name"].replace(/-/g," ") + "</h1></div>"
+				info[cur["name"]] = {"desc":cur["description"], "url":"https://keartland.github.io/"+cur["name"] }
+			}
 		}
-	})
+	}
+})
 
 function run(id){
 	var ele = document.getElementById(id)
@@ -24,7 +24,7 @@ function run(id){
 		temp = ele.innerHTML
 		ele.setAttribute('class', 'active');
 		ele.innerHTML = temp + add
-	}else if(ele.className == "active"){
+	} else if(ele.className == "active"){
 		ele.setAttribute('class', 'row');
 		ele.innerHTML = ele.innerHTML.replace(add,"")
 	}
