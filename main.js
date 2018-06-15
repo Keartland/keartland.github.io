7eadacf751faf5f109ff1c69fc440c5861232842
Harry Keartland <harrykeartland@gmail.com>
2018-06-15 14:21:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,23 +4,23 @@ fetch("https://api.github.com/users/keartland/repos?sort=%22updated%22").then(r 
       cur = data[i]
       lang = cur["language"]
       if(lang =="JavaScript" || lang =="CSS"|| lang =="HTML"){
-			if (cur["name"]!="keartland.github.io"){
-				document.getElementById("repos").innerHTML += "<div onclick=\"run(this.id)\" id=\"" + cur["name"] +"\" class=\"row\"><h1 class=\"titles\">" + cur["name"].replace(/-/g," ") + "</h1></div>"
-				info[cur["name"]] = {"desc":cur["description"], "url":cur["homepage"], "source":cur["html_url"] }
-			}
+				if (cur["name"]!="keartland.github.io"){
+        	document.getElementById("repos").innerHTML += "<div onclick=\"run(this.id)\" id=\"" + cur["name"] +"\" class=\"row\"><h1 class=\"titles\">" + cur["name"].replace(/-/g," ") + "</h1></div>"
+          console.log(cur)
+          info[cur["name"]] = {"desc":cur["description"], "url":cur["homepage"], "source":cur["html_url"], "download":cur["html_url"] + "/archive/master.zip" }
+      	}
     	}
-	}
-	
-})
+		}
+	})
 
 function run(id){
 	var ele = document.getElementById(id)
-	add = "<h2>" + info[id]["desc"] + "</h2><a href=\"" + info[id]["url"] +"\">GOTO</a><span>   </span><a href=\"" + info[id]["source"] +"\">Source</a>"
+	add = "<h2>" + info[id]["desc"] + "</h2><a href=\"" + info[id]["url"] +"\">GOTO</a><span>   </span><a href=\"" + info[id]["source"] +"\">Source</a><span>   </span><a href=\"" + info[id]["download"] +"\">Download</a>"
 	if(ele.className == "row"){
 		temp = ele.innerHTML
 		ele.setAttribute('class', 'active');
 		ele.innerHTML = temp + add
-	} else if(ele.className == "active"){
+	}else if(ele.className == "active"){
 		ele.setAttribute('class', 'row');
 		ele.innerHTML = ele.innerHTML.replace(add,"")
 	}
