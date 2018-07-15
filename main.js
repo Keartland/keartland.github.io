77e49cd3adc63717d0eab47b6b217c69bea4d58b
Harry Keartland <harrykeartland@gmail.com>
2018-07-15 16:20:24 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -4,13 +4,11 @@ fetch("https://api.github.com/users/keartland/repos?sort=%22updated%22").then(r 
     for (var i = 0; i < data.length; i++) {
       cur = data[i]
       lang = cur["language"]
-      if(lang == "JavaScript" || lang == "CSS" || lang == "HTML"){
-        document.getElementById("repos").innerHTML += "<div onclick=\"run(this.id)\" id=\"" + cur["name"] +"\" class=\"row\"><h1 class=\"titles\">" + cur["name"].replace(/-/g," ") + "</h1></div>"
-        info[cur["name"]] = { "desc":cur["description"], "url":cur["name"], "source":cur["html_url"], "download":cur["html_url"] + "/archive/master.zip" }
-        string += "window.open('" + info[cur["name"]]["download"] + "');"
-    	}
+      document.getElementById("repos").innerHTML += "<div onclick=\"run(this.id)\" id=\"" + cur["name"] +"\" class=\"row\"><h1 class=\"titles\">" + cur["name"].replace(/-/g," ") + "</h1></div>"
+      info[cur["name"]] = { "desc":cur["description"], "url":cur["name"],"isWeb":lang == "JavaScript" || lang == "CSS" || lang == "HTML", "source":cur["html_url"], "download":cur["html_url"] + "/archive/master.zip" }
+      string += "window.open('" + info[cur["name"]]["download"] + "');"
 		}
-    document.getElementById("repos").innerHTML += "<div onclick=\"window.open('https://projecteuler.net/problem='+(Math.floor(Math.random() * 600) + 1))\" class=\"row\"><h1 style=\"color:#eaf1ff\" class=\"titles\">Random Project Euler Question</h1></div>" 
+    document.getElementById("repos").innerHTML += "<div onclick=\"window.open('https://projecteuler.net/problem='+(Math.floor(Math.random() * 600) + 1))\" class=\"row\"><h1 style=\"color:#eaf1ff\" class=\"titles\">Random Project Euler Question</h1></div>"
     document.getElementById("repos").innerHTML += "<div onclick=\"if(window.confirm(\'Are you sure you want to download?\')){" + string + "} else{console.log(\'Canceled\');}\" class=\"row\"><h1 style=\"color:#eaf1ff\" class=\"titles\">Download all</h1></div>"
 })
 
@@ -24,7 +22,7 @@ function run(id){
 }
 function toggle(id, on){
   ele = document.getElementById(id)
-  add = "<h2>" + info[id]["desc"] + "</h2><a href=\"" + info[id]["url"] + "\">GOTO</a><span>   </span><a href=\"" + info[id]["source"] + "\">Source</a><span>   </span><a onclick=\"if(window.confirm(\'Are you sure you want to download?\')){window.open('" + info[id]["download"] + "');} else{console.log(\'Canceled\');}\" href=\"#\">Download</a>"
+  add = "<h2>" + info[id]["desc"] + "</h2>" + "<a href=\"" + (info[id]["isWeb"] ? info[id]["url"] + "\">GOTO</a><span>   </span>" : "")+ "<a href=\"" + info[id]["source"] + "\">Source</a><span>   </span><a onclick=\"if(window.confirm(\'Are you sure you want to download?\')){window.open('" + info[id]["download"] + "');} else{console.log(\'Canceled\');}\" href=\"#\">Download</a>"
   ele.className = on ? 'active' : 'row';
-  ele.innerHTML = on ? ele.innerHTML + add : ele.innerHTML.replace(add,"");
+  ele.innerHTML = on ? ele.innerHTML + add : "<h1 class=\"titles\">" +  info[id]["url"].replace(/-/g," ") + "</h1>";
 }
