2261747c69c8f245eb0e24dafd0523e15ce0a88a
Harry Keartland <harrykeartland@gmail.com>
2018-07-20 22:46:14 +0100

    made the background change might make it more gradual

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 info = {}
 string = ""
+colours = ["#49a078","#499ca0","#4970a0","#4d5da0","#7849a0","#a0499c","#a04970","#a04d49","#a07849","#9ca049","#70a049","#49a04d"]
+document.body.style.background = colours[0]
 fetch("https://api.github.com/users/keartland/repos?sort=%22updated%22").then(r => r.json()).then(function(data){
     for (var i = 0; i < data.length; i++) {
       cur = data[i]
@@ -11,7 +13,7 @@ fetch("https://api.github.com/users/keartland/repos?sort=%22updated%22").then(r 
     document.getElementById("repos").innerHTML += "<div onclick=\"window.open('https://projecteuler.net/problem='+(Math.floor(Math.random() * 600) + 1))\" class=\"row\"><h1 style=\"color:#eaf1ff\" class=\"titles\">Random Project Euler Question</h1></div>"
     document.getElementById("repos").innerHTML += "<div onclick=\"if(window.confirm(\'Are you sure you want to download?\')){" + string + "} else{console.log(\'Canceled\');}\" class=\"row\"><h1 style=\"color:#eaf1ff\" class=\"titles\">Download all</h1></div>"
 })
-
+counter = 0;
 function run(id){
   for(i=0;i < Object.keys(info).length;i++){
     if (Object.keys(info)[i] != id){
@@ -19,10 +21,19 @@ function run(id){
     }
   }
   toggle(id, document.getElementById(id).className != "active")
+  nextColour(colours)
 }
 function toggle(id, on){
   ele = document.getElementById(id)
   add = "<h2>" + info[id]["desc"] + "</h2>" + (info[id]["isWeb"] ? "<a href=\"" + info[id]["url"] + "\">GOTO</a><span>   </span>" : "")+ "<a href=\"" + info[id]["source"] + "\">Source</a><span>   </span><a onclick=\"if(window.confirm(\'Are you sure you want to download?\')){window.open('" + info[id]["download"] + "');} else{console.log(\'Canceled\');}\" href=\"#\">Download</a>"
   ele.className = on ? 'active' : 'row';
   ele.innerHTML = on ? ele.innerHTML + add : "<h1 class=\"titles\">" +  info[id]["url"].replace(/-/g," ") + "</h1>";
+}
+function nextColour(colours){
+  if (colours.length != counter) {
+    counter++;
+  } else {
+    counter = 0;
+  }
+  document.body.style.background = colours[counter];
 }
